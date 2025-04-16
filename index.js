c44c27b841e30939cad8e0d81c18d6cf5ed93237
DostonEliboyev <doston1721@gmail.com>
2025-04-16 10:49:21 +0500

    path and fs

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-const {user,userLogger} = require("./user");
-const {car,carLogger} = require("./car");
+const fs= require("fs");
+const path= require("path");
 
-userLogger(user);
-carLogger(car);
+// fs.mkdir(path.join(__dirname,"templates"),err =>{
+//     if(err) throw new Error();
+//     console.log("Folder created successfully");
+// })
+
+// fs.mkdir(path.join(__dirname,"Notes","users"),err=>{
+//     if(err) throw new Error();
+//     console.log("Folder created successfully");
+// })
+
+fs.writeFile(path.join(__dirname,"Notes","users","user1.txt"),"User 1",(err)=>{
+    if(err) throw new Error();
+    console.log("File created successfully");
+    fs.appendFile(path.join(__dirname,"Notes","users","user1.txt"),"User 2",(err)=>{
+        if(err) throw new Error();
+        console.log("File appended successfully");
+        fs.readFile(path.join(__dirname,"Notes","users","user1.txt"),"utf-8",(err,data)=>{
+            if(err) throw new Error();
+            console.log(data);
+        })
+    })
+})
+
+
+
+
+
+
+
 
 
