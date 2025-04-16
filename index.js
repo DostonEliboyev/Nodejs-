2a5a964ed34406af033323a9b52d88e5dabf8db3
DostonEliboyev <doston1721@gmail.com>
2025-04-16 15:53:05 +0500

    node

--- index.js
+++ index.js
@@ -1,34 +1,14 @@
-const fs= require("fs");
-const path= require("path");
-
-// fs.mkdir(path.join(__dirname,"templates"),err =>{
-//     if(err) throw new Error();
-//     console.log("Folder created successfully");
-// })
-
-// fs.mkdir(path.join(__dirname,"Notes","users"),err=>{
-//     if(err) throw new Error();
-//     console.log("Folder created successfully");
-// })
-
-fs.writeFile(path.join(__dirname,"Notes","users","user1.txt"),"User 1",(err)=>{
-    if(err) throw new Error();
-    console.log("File created successfully");
-    fs.appendFile(path.join(__dirname,"Notes","users","user1.txt"),"User 2",(err)=>{
-        if(err) throw new Error();
-        console.log("File appended successfully");
-        fs.readFile(path.join(__dirname,"Notes","users","user1.txt"),"utf-8",(err,data)=>{
-            if(err) throw new Error();
-            console.log(data);
-        })
+const http= require("http");
+const server= http.createServer((req,res)=>{
+    res.writeHead(200,{
+        "Content-Type":"text/html"
     })
+    res.write("<h1>Men birinchi serverim yasadim va ishlaydi nima gap</h1>")
+    res.end()
+})  
+
+server.listen(3000,"localhost",()=>{
+    console.log("Server is running on port 3000");
 })
-
-
-
-
-
-
-
 
 
